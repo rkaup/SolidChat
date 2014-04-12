d75e40c27be6ce9852c5f2451017bcc192733829
Robin Kaup <robin@robinkaup.com>
2014-04-12 02:00:42 +0100

    generate angular app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('solidChatApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
